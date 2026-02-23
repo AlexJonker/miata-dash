6498295cfdeb9617243ff3b2201e1d5d8c78086a
Alex Jonker <alex.jonker2008@gmail.com>
2026-02-24 00:32:39 +0100

    Improve build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 set -e
 
 cd buildroot
-echo "Cleaning old configs"
-rm -f .config
-rm -f .config.old
-rm -f ..config.tmp
 
 echo "Configuring Buildroot for Raspberry Pi 2"
 make raspberrypi2_defconfig
@@ -16,3 +12,18 @@ echo "Building image (this may take a while)"
 make -j$(nproc)
 
 echo "Build complete! The image is located at buildroot/output/images/sdcard.img"
+
+
+echo ""
+echo "================================"
+echo ""
+
+echo "Do you want to flash the image to an SD card now? (y/n)"
+read -r answer
+if [[ "$answer" == "y" ]]; then
+    cd ..
+    echo "Flashing image to SD card..."
+    ./flash.sh
+else
+    echo "You can flash the image later by running ./flash.sh"
+fi
