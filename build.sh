e7267e6132465c7341039d67cb30f001e623ff52
Alex Jonker <alex.jonker2008@gmail.com>
2026-02-24 23:37:21 +0100

    Fix build scripts

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cd modules/buildroot
 
 echo "Configuring Buildroot for Raspberry Pi 2"
 make raspberrypi2_defconfig
-cat ../config-overrides/rpi2 >> .config
+cat ../../config-overrides/rpi2 >> .config
 make olddefconfig
 
 echo "Building image (this may take a while)"
@@ -21,7 +21,7 @@ echo ""
 echo "Do you want to flash the image to an SD card now? (y/n)"
 read -r answer
 if [[ "$answer" == "y" ]]; then
-    cd ..
+    cd ../..
     echo "Flashing image to SD card..."
     ./flash.sh
 else
