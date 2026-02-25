d535f8f356e09f19d1c0c10185ba4c833302f0d8
Alex Jonker <alex.jonker2008@gmail.com>
2026-02-25 22:08:53 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,11 +6,9 @@ TARGET_DEVICE="${1:-rpi3}"
 case "$TARGET_DEVICE" in
     rpi2)
         DEFCONFIG="raspberrypi2_defconfig"
-        OVERRIDE_FILE="../../config-overrides/rpi2"
         ;;
     rpi3)
         DEFCONFIG="raspberrypi3_defconfig"
-        OVERRIDE_FILE="../../config-overrides/rpi3"
         ;;
     *)
         echo "Error: Unsupported target '$TARGET_DEVICE'. Use 'rpi2' or 'rpi3'."
@@ -23,7 +21,7 @@ cd modules/buildroot
 
 echo "Configuring Buildroot for ${TARGET_DEVICE}"
 make "$DEFCONFIG"
-cat "$OVERRIDE_FILE" >> .config
+cat "../../config-overrides/defconfig" >> .config
 make olddefconfig
 
 echo "Building image (this may take a while)"
