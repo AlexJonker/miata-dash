3aeb93fd63eba51a49756ef1a2c995771f59d8d0
Alex Jonker <alex.jonker2008@gmail.com>
2026-05-20 00:18:20 +0200

    Improve dash startup

---
+++ src/storage/boot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script is executed by systemd at boot time.
+
+source /storage/settings.conf
+export SLINT_BACKEND
+
+if [ "$ENABLE_DASH" = "true" ]; then
+    exec /usr/local/bin/dash # SLINT_BACKEND is set in settings.conf
+else
+    exec /sbin/agetty --autologin driver --noclear tty1 linux
+fi
